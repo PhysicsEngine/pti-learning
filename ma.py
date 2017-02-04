d4f2b7f7ca322bc2cfe22f7956e7122618c09f58
yamada <yamada@ik1-313-16678.vs.sakura.ne.jp>
2017-02-04 17:41:55 +0900

    :new: add stopwords

---
+++ ma.py
@@ -0,0 +1,38 @@
+#!/usr/local/anaconda3/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import MeCab
+import codecs
+
+STOPWORDS = ["。", ".", ".", "、", ",", "，"]
+SKIP_WORD_CLASSES = ["BOS/EOS", "助動詞", "助詞", "代名詞"]
+
+class MA:
+    def __init__(self):
+        self.mecab = MeCab.Tagger("-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+        self.stopwords = STOPWORDS
+        self.skip_word_classes  = SKIP_WORD_CLASSES
+        
+    def parse(self, sentence):
+        sentence = sentence.strip()
+        node = self.mecab.parseToNode(sentence)
+        res = []
+        while node:
+            cat1 = node.feature.split(",")[0]
+            if node.surface in STOPWORDS:
+                node = node.next
+                continue
+            if cat1 in SKIP_WORD_CLASSES:
+                node = node.next
+                continue
+            res.append(node.surface)
+            node = node.next
+        return " ".join(res)
+
+if __name__ == '__main__':
+    ma = MA()
+    with codecs.open("/var/pti/scrape/1.txt", "r", "utf-8") as f:
+        for line in f:
+            line = line.rstrip()
+            x = ma.parse(line)
+            print(x)
